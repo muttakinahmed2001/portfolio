cdb87b329433a23f67c15a33c27d1ea5304e4b8d
unknown <muttakinahmed871@gmail.com>
2024-03-02 02:04:12 +0600

    some change in skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,24 +2,22 @@ import Image from "next/image";
 
 const Skills = () => {
   const frontend = [
-    "/public/skills/html.jpg",
-    "/public/skills/css.jpg",
-    "/public/skills/bootstrap.jpg",
-    "/public/skills/tailwind.jpg",
-    "/skills/daisy.png",
-    "/skills/javascript.png",
-    "/skills/react.png",
-    "/skills/next.png",
+    "/skills/HTML5_Badge.svg",
+    "/skills/CSS3_logo.svg",
+    "/skills/Bootstrap_logo.svg",
+    "/skills/tailwind.jpg",
+    "/skills/Unofficial_JavaScript_logo_2.svg",
+    "/skills/React.svg",
+    "/skills/Nextjs-logo.svg",
+    "/skills/redux-logo-svgrepo-com.svg",
   ];
 
   const backend = [
-    "/skills/node.png",
-    "/skills/express.png",
-    "/skills/mongo.png",
+    "/skills/node-js-svgrepo-com.svg",
+    "/skills/express.svg",
+    "/skills/mongodb-svgrepo-com.svg",
+    "/skills/firebase-svgrepo-com.svg",
   ];
-
-  const firebase = ["/skills/firebase.png"];
-  const redux = ["/skills/redux.png"];
 
   return (
     <div className=" my-20 text-black">
@@ -31,13 +29,14 @@ const Skills = () => {
           </h1>
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-5  ">
             {frontend.map((skill, index) => (
-              <div key={index}>
+              <div className="flex justify-center items-center" key={index}>
                 <Image
                   className="object-cover"
                   width={100}
                   height={100}
                   alt="icon"
-                  src={skill}></Image>
+                  src={skill}
+                  layout="fixed"></Image>
               </div>
             ))}
           </div>
@@ -49,40 +48,6 @@ const Skills = () => {
 
           <div className="grid grid-cols-2 sm:grid-cols-3 gap-5 ">
             {backend.map((skill, index) => (
-              <div key={index}>
-                <Image
-                  className="object-cover"
-                  width={100}
-                  height={100}
-                  alt="icon"
-                  src={skill}></Image>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-xl  sm:text-3xl text-center font-bold mb-10">
-            Authentication
-          </h1>
-          <div>
-            {firebase.map((skill, index) => (
-              <div key={index}>
-                <Image
-                  className="object-cover"
-                  width={100}
-                  height={100}
-                  alt="icon"
-                  src={skill}></Image>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="flex flex-col justify-center items-center">
-          <h1 className="text-xl  sm:text-3xl text-center font-bold mb-10">
-            State Management
-          </h1>
-          <div>
-            {redux.map((skill, index) => (
               <div key={index}>
                 <Image
                   className="object-cover"
